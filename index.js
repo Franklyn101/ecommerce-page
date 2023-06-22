1477acf7a788c2e908bd0ba7be3d2fcd0534adca
Franklyn inoma <125371243+Franklyn101@users.noreply.github.com>
2023-06-22 13:22:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ minus.addEventListener('click', ()=> {
     count = 0
   }
  value.textContent = count
-
+ localStorage.setItem('count',count)
 });
 
 
@@ -39,7 +39,8 @@ minus.addEventListener('click', ()=> {
 add.addEventListener('click', ()=> {
   count++;
   value.textContent = count
-
+  localStorage.setItem('count',count)
+  localStorage.setItem('status','added')
 });
 
 //   };
@@ -87,6 +88,49 @@ add.addEventListener('click', ()=> {
 
     })
 
+    let body = document.querySelector('body')
+   body.addEventListener('mouseover',()=>{
+    count = localStorage.getItem('count');
+      value.textContent = count
+      let badge = document.querySelector('.badge')
+      let total = document.querySelector('.total')
+      let deletes = document.querySelector('.delete')
+      let together = document.querySelector('.together')
+      let noOfItems = document.querySelector('.no-of-items')
+      deletes.addEventListener('click',()=>{
+        together.textContent = 'Your Cart is Empty'
+        together.classList.add('margin')
+        localStorage.setItem('status','deleted')
+      })
+      if (localStorage.getItem('status') == 'deleted') {
+        together.textContent = 'Your Cart is Empty'
+        together.classList.add('margin')
+        count = 0
+        value.textContent = count
+ localStorage.setItem('count',count)
+      } else {
+        together.innerHtml = `    <div class="cart-product">
+        <img class="item" src="/image-product-1-thumbnail.jpg" alt="">
+       <p> Fall Limited Sneakers Edition <span>$125.00 x <span class="no-of-items">3</span>  <b class="total">$375.00</b></span>
+       </p>
+       <img class="delete" src="/icon-delete.svg" alt="">
+  
+      </div>
+      <span class="checkout">Checkout</span>`
+      }
+      if (count >= 1) {
+        badge.textContent = 1
+        badge.style.display = 'grid'
+        noOfItems.textContent = count
+        total.textContent = `$${125 * count}`
+      } else {
+        badge.style.display = 'none'
+        together.textContent = 'Your Cart is Empty'
+        together.classList.add('margin')
+      }
+      
+    })
+       
 
 
 //   hamburger
@@ -123,31 +167,3 @@ add.addEventListener('click', ()=> {
     
   });
 
-
-  
-
-  let hamburger = document.querySelector('.hamburger');
-    let menu = document.querySelector('.menu');
-    let isActive = false;
-
-    function toggleMenu() {
-      isActive = !isActive; // Toggle the active state
-
-      hamburger.classList.toggle('active');
-      menu.classList.toggle('active');
-
-      if (isActive) {
-        document.addEventListener('click', closeMenu);
-      } else {
-        document.removeEventListener('click', closeMenu);
-      }
-    }
-
-    function closeMenu(event) {
-      if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
-        isActive = false;
-        hamburger.classList.remove('active');
-        menu.classList.remove('active');
-        document.removeEventListener('click', closeMenu);
-      }
-    }
